72be039a16b6967d3977ba36fa2ceb110a7bb4f4
Laiqh <cute_penguin@mail.ru>
2020-02-01 13:25:15 +0200

    Bugfixing DiscountLessThanOne validation rule.

--- src/com/javaguru/shoppinglist/service/validation/DiscountLessThanOne.java
+++ src/com/javaguru/shoppinglist/service/validation/DiscountLessThanOne.java
@@ -6,7 +6,7 @@ import java.math.BigDecimal;
 
 public class DiscountLessThanOne implements ValidationRule<Product> {
     public void validate(Product product) throws ValidationException {
-        if (product.getDiscount().compareTo(BigDecimal.ONE) > 1) {
+        if (product.getDiscount().compareTo(BigDecimal.ONE) == 1) {
             throw new ValidationException("Discount can't me greater than 1");
         }
     }
