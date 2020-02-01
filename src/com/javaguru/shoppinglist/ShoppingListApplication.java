e9edb33ad50153d9ddf3a6b2b8e6398961dadaed
Laiqh <cute_penguin@mail.ru>
2020-02-01 12:58:20 +0200

    Refactoring main ShoppingListApplication under SRP.

--- src/com/javaguru/shoppinglist/ShoppingListApplication.java
+++ src/com/javaguru/shoppinglist/ShoppingListApplication.java
@@ -1,83 +1,32 @@
 package com.javaguru.shoppinglist;
 
 import com.javaguru.shoppinglist.domain.Product;
-
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import com.javaguru.shoppinglist.repository.InMemoryRepository;
+import com.javaguru.shoppinglist.repository.Repository;
+import com.javaguru.shoppinglist.service.ProductService;
+import com.javaguru.shoppinglist.service.validation.ProductValidationRule;
+import com.javaguru.shoppinglist.service.validation.ValidationRule;
+import com.javaguru.shoppinglist.ui.ConsoleUI;
 
 class ShoppingListApplication {
+    private Repository<Product> repository;
+    private ValidationRule<Product> validationRule;
+    private ProductService service;
+    private ConsoleUI ui;
 
     public static void main(String[] args) {
-        Map<Long, Product> productRepository = new HashMap<>();
-        Long productIdSequence = 0L;
-        while (true) {
-            Scanner scanner = new Scanner(System.in);
-            try {
-                System.out.println("1. Create product");
-                System.out.println("2. Find product by id");
-                System.out.println("3. Exit");
-                Integer userInput = Integer.valueOf(scanner.nextLine());
-                switch (userInput) {
-                    case 1:
-                        System.out.println("Enter product name: ");
-                        String name = scanner.nextLine();
-                        if (!nameIsValid(name)) {
-                            System.out.println("Name is invalid");
-                            break;
-                        }
-                        System.out.println("Enter product category: ");
-                        String category = scanner.nextLine();
-                        System.out.println("Enter product price: ");
-                        BigDecimal price = new BigDecimal(scanner.nextLine());
-                        if (!priceIsValid(price)) {
-                            System.out.println("Price is invalid");
-                            break;
-                        }
-                        System.out.println("Enter product discount: ");
-                        BigDecimal discount = new BigDecimal(scanner.nextLine());
-                        if (!discountIsValid(discount)) {
-                            System.out.println("Discount is invalid");
-                            break;
-                        }
-                        System.out.println("Enter product description: ");
-                        String description = scanner.nextLine();
-                        Product product = new Product();
-                        product.setName(name);
-                        product.setCategory(category);
-                        product.setPrice(price);
-                        product.setDiscount(discount);
-                        product.setDescription(description);
-                        product.setId(productIdSequence);
-                        productRepository.put(productIdSequence, product);
-                        productIdSequence++;
-                        System.out.println("Result: " + product.getId());
-                        break;
-                    case 2:
-                        System.out.println("Enter product id: ");
-                        long id = scanner.nextLong();
-                        Product findProductResult = productRepository.get(id);
-                        System.out.println(findProductResult);
-                        break;
-                    case 3:
-                        return;
-                }
-            } catch (Exception e) {
-                System.out.println("Error! Please try again.");
-            }
-        }
+        ShoppingListApplication application = new ShoppingListApplication();
+        application.execute();
     }
 
-    private static boolean priceIsValid(BigDecimal price) {
-        return price.compareTo(BigDecimal.ZERO) > 0;
+    public ShoppingListApplication() {
+        this.repository = new InMemoryRepository<>();
+        this.validationRule = new ProductValidationRule();
+        this.service = new ProductService(validationRule, repository);
+        this.ui = new ConsoleUI(service);
     }
 
-    private static boolean discountIsValid(BigDecimal discount) {
-        return discount.compareTo(BigDecimal.ONE) < 0;
-    }
-
-    private static boolean nameIsValid(String name) {
-        return name.length() >= 3 && name.length() <= 32;
+    private void execute() {
+        this.ui.execute();
     }
 }
