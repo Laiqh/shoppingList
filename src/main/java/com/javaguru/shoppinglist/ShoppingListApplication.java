7564b2903fa0aedc852f299c3e0a60b1ca22cafb
Laiqh <cute_penguin@mail.ru>
2020-02-09 20:54:16 +0200

    Refarctoring product validation service, setting up rules in main application.

--- src/main/java/com/javaguru/shoppinglist/ShoppingListApplication.java
+++ src/main/java/com/javaguru/shoppinglist/ShoppingListApplication.java
@@ -4,11 +4,15 @@ import com.javaguru.shoppinglist.domain.Product;
 import com.javaguru.shoppinglist.repository.InMemoryRepository;
 import com.javaguru.shoppinglist.repository.Repository;
 import com.javaguru.shoppinglist.service.ProductService;
+import com.javaguru.shoppinglist.service.validation.*;
 import com.javaguru.shoppinglist.ui.ConsoleUI;
+
+import java.math.BigDecimal;
 
 class ShoppingListApplication {
     private Repository<Product> repository;
     private ProductService service;
+    private ProductValidationRule rules;
     private ConsoleUI ui;
 
     public static void main(String[] args) {
@@ -17,9 +21,26 @@ class ShoppingListApplication {
     }
 
     public ShoppingListApplication() {
-        this.repository = new InMemoryRepository<>();
-        this.service = new ProductService(repository);
-        this.ui = new ConsoleUI(service);
+        repository = new InMemoryRepository<>();
+
+        rules = new ProductValidationRule();
+        rules.add(new NotNull());
+
+        rules.add(new NameNotNull());
+        rules.add(new NameTooShort(3));
+        rules.add(new NameTooLong(32));
+        rules.add(new NameUnique(repository));
+
+        rules.add(new PriceNotNull());
+        rules.add(new PriceGreaterThanZero());
+
+        rules.add(new DiscountNotNull());
+        rules.add(new DiscountLessThanOrEqualsToOne());
+        rules.add(new DiscountGreaterThanOrEqualsToZero());
+        rules.add(new DiscountMinPrice(new BigDecimal("20")));
+
+        service = new ProductService(rules, repository);
+        ui = new ConsoleUI(service);
     }
 
     private void execute() {
--- src/main/java/com/javaguru/shoppinglist/service/ProductService.java
+++ src/main/java/com/javaguru/shoppinglist/service/ProductService.java
@@ -2,7 +2,6 @@ package com.javaguru.shoppinglist.service;
 
 import com.javaguru.shoppinglist.domain.Product;
 import com.javaguru.shoppinglist.repository.Repository;
-import com.javaguru.shoppinglist.service.validation.ProductValidationService;
 import com.javaguru.shoppinglist.service.validation.ValidationException;
 import com.javaguru.shoppinglist.service.validation.ValidationRule;
 
@@ -10,9 +9,9 @@ public class ProductService {
     private ValidationRule<Product> validationRule;
     private Repository<Product> repository;
 
-    public ProductService(Repository repository) {
+    public ProductService(ValidationRule rules, Repository repository) {
         this.repository = repository;
-        this.validationRule = new ProductValidationService(repository);
+        this.validationRule = rules;
     }
 
     public Long add(Product product) throws ValidationException {
