b93ffb75725b650c7c341e6d2ea16ac83ad54960
Laiqh <cute_penguin@mail.ru>
2020-02-14 20:38:49 +0200

    Update tests to use mockito annotations.

--- src/test/java/com/javaguru/shoppinglist/service/validation/AbstractValidationRuleTest.java
+++ src/test/java/com/javaguru/shoppinglist/service/validation/AbstractValidationRuleTest.java
@@ -1,13 +1,18 @@
 package com.javaguru.shoppinglist.service.validation;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@ExtendWith(MockitoExtension.class)
 class AbstractValidationRuleTest {
     private ValidationRule ruleVictim = Mockito.mock(AbstractValidationRule.class, Mockito.CALLS_REAL_METHODS);
-    private ValidationRule ruleToAdd = Mockito.mock(ValidationRule.class);
+    @Mock
+    private ValidationRule ruleToAdd;
 
     @Test
     public void addShouldThrowException() {
