a9545d5c95db516cd15b879cb54131035ebd3551
Laiqh <cute_penguin@mail.ru>
2020-02-14 21:06:00 +0200

    ProductService tests.

---
+++ src/test/java/com/javaguru/shoppinglist/service/ProductServiceTest.java
@@ -0,0 +1,46 @@
+package com.javaguru.shoppinglist.service;
+
+import com.javaguru.shoppinglist.domain.Product;
+import com.javaguru.shoppinglist.repository.Repository;
+import com.javaguru.shoppinglist.service.validation.ValidationException;
+import com.javaguru.shoppinglist.service.validation.ValidationRule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+    @Mock
+    private ValidationRule<Product> validationRule;
+    @Mock
+    private Repository<Product> repository;
+    @InjectMocks
+    ProductService service;
+
+    Product product;
+
+    @BeforeEach
+    public void init() {
+        product = new Product();
+    }
+
+    @Test
+    public void add() throws ValidationException {
+        service.add(product);
+
+        verify(validationRule).validate(product);
+        verify(repository).insert(product);
+    }
+
+    @Test
+    public void findById() {
+        service.findById(0L);
+
+        verify(repository).get(0L);
+    }
+}
